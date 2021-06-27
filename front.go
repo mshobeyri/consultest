1d04d70e09f278a0b72d54d95faf9f5b9142de93
mehrdad <mehrdad.shobeyri@gmail.com>
2021-06-27 23:15:23 +0400

    resolve grpc problem

--- front.go
+++ front.go
@@ -29,24 +29,25 @@ func main() {
 		}
 		defer conn.Close()
 		c := pb.NewCountingClient(conn)
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 
 		res, err := c.GetMessage(ctx, &pb.Message{
 			Text:   "Front",
 			Number: i,
 		})
-		if err != nil {
-			fmt.Fprintf(w, "could not greeting: %v", err)
 
+		i++
+
+		if err != nil {
+			fmt.Fprintf(w, "could not get message: %v", err)
+			return
 		}
 		fmt.Fprintf(w, "Front Message : %v %v\n", res.GetText(), res.GetNumber())
-		i++
 
 	})
 
 	fmt.Printf("server_addr: %s \n", os.Getenv("SERVER_ADDR"))
 	fmt.Printf("front server is listening on 8081")
 	log.Fatal(http.ListenAndServe(":8081", nil))
-
 }
--- makefile
+++ makefile
@@ -18,3 +18,5 @@ bpb:
 	protoc --go_out=. --go_opt=paths=source_relative \
     	--go-grpc_out=. --go-grpc_opt=paths=source_relative \
     	pb/counting.proto
+
+# rpc error: code = Unavailable desc = upstream connect error or disconnect/reset before headers. reset reason: connection termination
